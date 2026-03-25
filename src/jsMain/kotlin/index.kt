2b43cd03cdfae7c2b5c73f8b443eabcb16f5b23e
PeekAndPoke <kontakt@karsten-gerber.de>
2026-03-25 20:45:05 +0100

    version upgrade - ultra 0.103.0

--- src/jsMain/kotlin/index.kt
+++ src/jsMain/kotlin/index.kt
@@ -1,7 +1,7 @@
 package io.peekandpoke.kraft.examples.helloworld
 
-import de.peekandpoke.kraft.kraftApp
-import de.peekandpoke.kraft.vdom.preact.PreactVDomEngine
+import io.peekandpoke.kraft.kraftApp
+import io.peekandpoke.kraft.vdom.preact.PreactVDomEngine
 import kotlinx.html.h1
 
 val kraft = kraftApp {
